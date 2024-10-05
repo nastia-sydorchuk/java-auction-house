68736c1eed1d8236a5a7be542551e6a57efdbb24
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-10-05 18:35:18 +0100

    added class FurnitureList #2

---
+++ AuctionHouse/src/FurnitureList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class FurnitureList {
+    private ArrayList<Furniture> furnitureList;
+
+    //create an empty arraylist
+    public FurnitureList() {
+        this.furnitureList = new ArrayList<Furniture> ();
+    }
+}
