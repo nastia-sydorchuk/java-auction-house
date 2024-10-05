acdc8b3786b5cab9c8acfbd6293723c7d8404cbe
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-10-05 21:09:46 +0100

    created instances of furniture and populated FurnitureList, added query methods for the cheapest/most expensive price, newest/oldest items IDs #3

--- AuctionHouse/src/FurnitureList.java
+++ AuctionHouse/src/FurnitureList.java
@@ -7,4 +7,65 @@ public class FurnitureList {
     public FurnitureList() {
         this.furnitureList = new ArrayList<Furniture> ();
     }
+
+    // populate furniture list with some mock data
+    public void populate() {
+        Furniture furniture1 = new Furniture ("chair", "modern", "JL", 10, 15,20, 2022, "JL", "new", 30, 1);
+        this.furnitureList.add(furniture1);
+
+        Furniture furniture2 = new Furniture ("desk", "modern", "JL", 15,20,25,2023, "JL", "new", 40, 2);
+        this.furnitureList.add(furniture2);
+
+        Furniture furniture3 = new Furniture ("sofa","modern", "JL", 20,25,30,2024, "JL", "new", 200, 3);
+        this.furnitureList.add(furniture3);
+    }
+
+    //return number of items in furnitureList
+    public int getSize() {
+        return this.furnitureList.size();
+    }
+
+    //return the oldest item id in furnitureList
+    public int findOldestItemId() {
+        Furniture oldestItem = this.furnitureList.getFirst();
+        for (Furniture f : this.furnitureList) {
+            if (oldestItem.getYearOfOrigin() > f.getYearOfOrigin())
+                oldestItem = f;
+        }
+
+        return oldestItem.getId();
+    }
+
+    //return the newest item id in furnitureList
+    public int findNewestItemId() {
+        Furniture newestItem = this.furnitureList.getFirst();
+        for (Furniture f : this.furnitureList) {
+            if (newestItem.getYearOfOrigin() < f.getYearOfOrigin())
+                newestItem = f;
+        }
+
+        return newestItem.getId();
+    }
+
+    //return the cheapest price in furnitureList
+    public float findCheapestPrice() {
+        float cheapestPrice = this.furnitureList.getFirst().getStartingPrice();
+        for (Furniture f : this.furnitureList) {
+            if (cheapestPrice > f.getStartingPrice())
+                cheapestPrice = f.getStartingPrice();
+        }
+
+        return cheapestPrice;
+    }
+
+    //return the most expensive price in furnitureList
+    public float findMostExpensivePrice() {
+        float mostExpensivePrice = this.furnitureList.getFirst().getStartingPrice();
+        for (Furniture f : this.furnitureList) {
+            if (mostExpensivePrice < f.getStartingPrice())
+                mostExpensivePrice = f.getStartingPrice();
+        }
+
+        return mostExpensivePrice;
+    }
 }
--- AuctionHouse/src/Main.java
+++ AuctionHouse/src/Main.java
@@ -1,6 +1,30 @@
 public class Main{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        FurnitureList furnitureList = new FurnitureList();
+
+        furnitureList.populate();
+
+        //get number of items
+        int numberOfItems = furnitureList.getSize();
+        System.out.println(numberOfItems);
+
+        //get oldest item id
+        int oldestItemId = furnitureList.findOldestItemId();
+        System.out.println("The oldest item is with id " + oldestItemId);
+
+        //get newest item id
+        int newestItemId = furnitureList.findNewestItemId();
+        System.out.println("The newest item is with id " + newestItemId);
+
+        //get the cheapest price
+        float cheapestPrice = furnitureList.findCheapestPrice();
+        System.out.println("The cheapest price is " + cheapestPrice);
+
+        //get the most expensive price
+        float mostExpensivePrice = furnitureList.findMostExpensivePrice();
+        System.out.println("The cheapest price is " + mostExpensivePrice);
+
         System.out.println("Hello Auction House");
     }
 }
