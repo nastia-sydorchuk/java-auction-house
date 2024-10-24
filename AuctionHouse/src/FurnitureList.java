223a140992107b9f308fd6104370c4f5feff515e
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-10-24 18:31:08 +0100

    populating furnitureList from the file #4

--- AuctionHouse/src/FurnitureList.java
+++ AuctionHouse/src/FurnitureList.java
@@ -1,4 +1,7 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class FurnitureList {
     private ArrayList<Furniture> furnitureList;
@@ -8,16 +11,34 @@ public class FurnitureList {
         this.furnitureList = new ArrayList<Furniture> ();
     }
 
-    // populate furniture list with some mock data
-    public void populate() {
-        Furniture furniture1 = new Furniture ("chair", "modern", "JL", 10, 15,20, 2022, "JL", "new", 30, 1);
-        this.furnitureList.add(furniture1);
+    // populate furniture list with data from the file
+    public void populate(String furnitureFilename) {
+        File f = new File(furnitureFilename);
+        try (Scanner scanner = new Scanner(f)) {
+            FurnitureParser parser = new FurnitureParser();
+            while (scanner.hasNextLine()) {
+                String inputLine = scanner.nextLine();
+                Furniture furniture = parser.parseFurnitureFromLine(inputLine);
+                this.addOneFurniture(furniture);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-        Furniture furniture2 = new Furniture ("desk", "modern", "JL", 15,20,25,2023, "JL", "new", 40, 2);
-        this.furnitureList.add(furniture2);
+    public void addOneFurniture(Furniture f) {
+        int id = f.getId();
+        Furniture inList = this.findById(id);
+        if (inList == null) {
+            this.furnitureList.add(f);
+        }
+    }
 
-        Furniture furniture3 = new Furniture ("sofa","modern", "JL", 20,25,30,2024, "JL", "new", 200, 3);
-        this.furnitureList.add(furniture3);
+    public Furniture findById(int id){
+        for (Furniture f : furnitureList)
+            if (f.getId() == id)
+                return f;
+        return null;
     }
 
     //return number of items in furnitureList
--- AuctionHouse/src/Main.java
+++ AuctionHouse/src/Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args) {
         FurnitureList furnitureList = new FurnitureList();
 
-        furnitureList.populate();
+        furnitureList.populate("AuctionHouse/datasets/furniture_items.csv");
 
         //get number of items
         int numberOfItems = furnitureList.getSize();
