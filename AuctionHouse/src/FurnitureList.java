79a4858b5ef5a81cff7b1867389d3493be77222b
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-10-24 21:39:38 +0100

    added missing statistics summary methods #5

--- AuctionHouse/src/FurnitureList.java
+++ AuctionHouse/src/FurnitureList.java
@@ -90,17 +90,91 @@ public class FurnitureList {
         return mostExpensiveItem;
     }
 
+    // return average of an array of numbers
+    public double calculateMean(float[] numbers) {
+        double sum = 0.0;
+        for (double num : numbers) {
+            sum += num;
+        }
+        return sum / numbers.length;
+    }
+
+    // return average deviation
+    public double calculateAverageDeviation(float[] numbers) {
+        double mean = calculateMean(numbers);
+        double sumOfDeviations = 0.0;
+        for (double num : numbers) {
+            sumOfDeviations += Math.abs(num - mean);
+        }
+
+        return sumOfDeviations / numbers.length;
+    }
+
+    // return standard deviation
+    public double calculateStandardDeviation(float[] prices) {
+        int n = prices.length;
+        double mean = this.calculateMean(prices);
+        double variance = 0;
+        for (Float price : prices) {
+            variance += Math.pow(price - mean, 2);
+        }
+        variance /= n;
+
+        return Math.sqrt(variance);
+    }
+
+    public float[] extractPrices() {
+        int length = this.getSize();
+        float[] prices = new float[length];
+        for (int i = 0; i < length; i++) {
+            prices[i] = this.furnitureList.get(i).getStartingPrice();
+        }
+
+        return prices;
+    }
+
+    public String getBreakdownByCondition() {
+        // Initialize counters for each condition
+        int newCount = 0;
+        int goodCount = 0;
+        int damagedCount = 0;
+
+        for (Furniture f : furnitureList) {
+            String condition = f.getCondition();
+
+            switch (condition) {
+                case "New":
+                    newCount++;
+                    break;
+                case "Good":
+                    goodCount++;
+                    break;
+                case "Damaged":
+                    damagedCount++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return "New: " + newCount + "\n" + "Good: " + goodCount + "\n" + "Damaged: " + damagedCount + "\n";
+    }
+
     public String createReport() {
         Furniture oldestItem = this.findOldestItem();
         Furniture newestItem = this.findNewestItem();
         Furniture leastExpensiveItem = this.findLeastExpensiveItem();
         Furniture mostExpensiveItem = this.findMostExpensivePrice();
+        float[] startingPricesArray = this.extractPrices();
 
         String report = "Total number of items in inventory is " + this.getSize() + "\n";
         report += "The oldest item is under ID " + oldestItem.getId() + ", year of origin: " + oldestItem.getYearOfOrigin() + "\n";
         report += "The newest item is under ID " + newestItem.getId() + ", year of origin: " + newestItem.getYearOfOrigin() + "\n";
         report += "The least expensive item is under ID " + leastExpensiveItem.getId() + ", starting price: " + leastExpensiveItem.getStartingPrice() + "\n";
         report += "The most expensive item is under ID " + mostExpensiveItem.getId() + ", starting price: " + mostExpensiveItem.getStartingPrice() + "\n";
+        report += "Breakdown of items by condition:\n" + this.getBreakdownByCondition();
+        report += "The average deviation of starting prices is " + this.calculateAverageDeviation(startingPricesArray) + "\n";
+        report += "The standard deviation of starting prices is " + this.calculateStandardDeviation(startingPricesArray) + "\n";
 
         return report;
     }
