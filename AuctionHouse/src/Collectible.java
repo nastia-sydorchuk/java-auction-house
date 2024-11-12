9f3ef71de8b1afa03d512f55bed4548975500fb0
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-12 23:17:25 +0000

    added a Collectible class #6

---
+++ AuctionHouse/src/Collectible.java
@@ -0,0 +1,41 @@
+public abstract class Collectible {
+    //instance variables
+    private int yearOfOrigin;
+    private String owner;
+    private String condition;
+    private float startingPrice;
+    private int id;
+
+    protected Collectible(int yearOfOrigin, String owner, String condition, float startingPrice, int id) {
+        this.yearOfOrigin = yearOfOrigin;
+        this.owner = owner;
+        this.condition = condition;
+        this.startingPrice = startingPrice;
+        this.id = id;
+    }
+
+    //Return yearOfOrigin
+    public int getYearOfOrigin() {
+        return yearOfOrigin;
+    }
+
+    //Return owner
+    public String getOwner() {
+        return owner;
+    }
+
+    //Return condition
+    public String getCondition() {
+        return condition;
+    }
+
+    //Return startingPrice
+    public float getStartingPrice() {
+        return startingPrice;
+    }
+
+    //Return id
+    public int getId() {
+        return id;
+    }
+}
