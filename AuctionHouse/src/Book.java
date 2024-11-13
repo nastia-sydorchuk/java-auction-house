74f19c3a25429859d77ce9be7ca40e7f5e48a762
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-13 22:07:57 +0000

    added class Book #7

---
+++ AuctionHouse/src/Book.java
@@ -0,0 +1,36 @@
+public class Book extends Collectible {
+    //instance variables
+    private String title;
+    private String edition;
+    private String genre;
+    private String authorsName;
+
+    //constructor
+    public Book(String title, String edition, String genre, String authorsName, int yearOfOrigin, String owner, String condition, float startingPrice, int id) {
+        super(yearOfOrigin, owner, condition, startingPrice, id);
+        this.title = title;
+        this.edition = edition;
+        this.genre = genre;
+        this.authorsName = authorsName;
+    }
+
+    //Return title
+    public String getTitle() {
+        return title;
+    }
+
+    //Return edition
+    public String getEdition() {
+        return edition;
+    }
+
+    //Return genre
+    public String getGenre() {
+        return genre;
+    }
+
+    //Return authorsName
+    public String getAuthorsName() {
+        return authorsName;
+    }
+}
