3a82a7c5935db28734389e2c57f4dd3a49ecf124
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-13 22:08:34 +0000

    added class Coin #8

---
+++ AuctionHouse/src/Coin.java
@@ -0,0 +1,35 @@
+public class Coin extends Collectible {
+    private String material;
+    private String placeOfOrigin;
+    private float originalValue;
+    private String currency;
+
+    //constructor
+    public Coin(String material, String placeOfOrigin, float originalValue, String currency, int yearOfOrigin, String owner, String condition, float startingPrice, int id) {
+        super(yearOfOrigin, owner, condition, startingPrice, id);
+        this.material = material;
+        this.placeOfOrigin = placeOfOrigin;
+        this.originalValue = originalValue;
+        this.currency = currency;
+    }
+
+    //Return material
+    public String getMaterial() {
+        return material;
+    }
+
+    //Return placeOfOrigin
+    public String getPlaceOfOrigin() {
+        return placeOfOrigin;
+    }
+
+    //Return originalValue
+    public float getOriginalValue() {
+        return originalValue;
+    }
+
+    //Return currency
+    public String getCurrency() {
+        return currency;
+    }
+}
