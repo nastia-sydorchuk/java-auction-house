7d861d1699775817209c72e2d04899bee73a82e2
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-13 22:08:55 +0000

    added class Car #9

---
+++ AuctionHouse/src/Car.java
@@ -0,0 +1,28 @@
+public class Car extends Collectible {
+    private String make;
+    private String model;
+    private boolean hasBeenServiced;
+
+    //constructor
+    public Car(String make, String model, boolean hasBeenServiced, int yearOfOrigin, String owner, String condition, float startingPrice, int id) {
+        super(yearOfOrigin, owner, condition, startingPrice, id);
+        this.make = make;
+        this.model = model;
+        this.hasBeenServiced = hasBeenServiced;
+    }
+
+    //Return make
+    public String getMake() {
+        return make;
+    }
+
+    //Return model
+    public String getModel() {
+        return model;
+    }
+
+    //Return hasBeenServiced
+    public boolean getHasBeenServiced() {
+        return hasBeenServiced;
+    }
+}
