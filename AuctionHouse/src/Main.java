2653274c8cdb55659d1f27a166a65b7f74f1bfce
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-14 22:57:41 +0000

    updated data files #10

--- AuctionHouse/src/Main.java
+++ AuctionHouse/src/Main.java
@@ -4,11 +4,11 @@ public class Main{
         CollectibleList collectibleList = new CollectibleList();
 
         // populate with proper data
-        collectibleList.populate("AuctionHouse/datasets/furniture_items.csv");
+        //collectibleList.populate("AuctionHouse/datasets/furniture_items.csv");
         // populate with missing fields data
-        // furnitureList.populate("AuctionHouse/datasets/furniture_items_missing_fields.csv");
+        collectibleList.populate("AuctionHouse/datasets/furniture_items_missing_fields.csv");
         // populate with wrongly typed data
-        // furnitureList.populate("AuctionHouse/datasets/furniture_items_wrong_types.csv");
+        //collectibleList.populate("AuctionHouse/datasets/furniture_items_wrong_types.csv");
 
         System.out.println(collectibleList);
 
