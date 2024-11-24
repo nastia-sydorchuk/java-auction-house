cefae0e29ba8ac47fd93db66014f17d4ca11c36d
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2024-11-24 21:22:17 +0000

    added a button to display more info #13

--- AuctionHouse/src/AuctionHouseGUI.java
+++ AuctionHouse/src/AuctionHouseGUI.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
-public class AuctionHouseGUI extends JFrame {
+public class AuctionHouseGUI extends JFrame implements ActionListener {
+    private JButton moreInfo;
     private DefaultListModel<Collectible> collectibleListModel;
     private JList<Collectible> collectibleList;
 
@@ -14,7 +17,7 @@ public class AuctionHouseGUI extends JFrame {
         this.setSize(400,400);
         this.setLocation(100,100);
         this.makeLayout();
-
+        this.setClicks();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -28,11 +31,34 @@ public class AuctionHouseGUI extends JFrame {
     //generate the UI layout
     private void makeLayout(){
         // instantiate components
+        moreInfo = new JButton("More info");
         collectibleList = new JList<>();
         // create containers
+        JPanel bottom = new JPanel();
         JScrollPane scrollList = new JScrollPane(collectibleList);
+        // add components to containers
+        bottom.add(moreInfo);
         // add containers to frame
+        this.add(bottom, BorderLayout.SOUTH);
         this.add(scrollList, BorderLayout.CENTER);
+    }
+
+    //private method adding click commands to buttons
+    private void setClicks(){
+        moreInfo.setActionCommand("moreInfo");
+        moreInfo.addActionListener(this);
+    }
+
+    //event handler for the button clicks
+    public void actionPerformed(ActionEvent event){
+        String command = event.getActionCommand();
+        if(command.equals("moreInfo")
+                && !collectibleList.isSelectionEmpty()){
+            Collectible c = collectibleList.getSelectedValue();
+            String message = c.getDetails();
+            JOptionPane.showMessageDialog(this, message, "Collectible details", JOptionPane.INFORMATION_MESSAGE);
+            collectibleList.updateUI();
+        }
     }
 
      //command to set the content of the device list
